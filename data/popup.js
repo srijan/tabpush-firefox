c5451e9de8fc04752b0d097166746db66ff492b5
Srijan Choudhary <srijan4@gmail.com>
2012-03-25 17:00:19 +0530

    UI improvements to popup.

--- data/popup.js
+++ data/popup.js
@@ -12,7 +12,10 @@ document.getElementById("sendButton").onclick = (function() {
 
 self.port.on("addTab", function(tab) {
   var item = document.createElement("li");
-  item.innerHTML = "<a href=\""+tab.url+"\" onclick=\"return false;\">"+tab.title+"</a> -- "+tab.sender;
+  item.innerHTML =  "<span class=\"selector\">&nbsp;</span>"  +
+                    "<a href=\""+tab.url+"\" onclick=\"return false;\">"  +
+                    "<span class=\"sender\">"+tab.sender+"</span>"  +
+                    "<span class=\"link\">"+tab.title+"</span></a>";
   document.getElementById("inboxList").appendChild(item);
   item.onclick = function() {
     self.port.emit("openTab",tab.url);
